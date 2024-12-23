cb26a9b92ac618b195f5f126954dd257bdd35e44
anhtran <anhtr077@gmail.com>
2024-12-23 15:43:33 -0800

    adding more data in seed.py and update all the route: get all, get one, delete one, update one, post one card for card_routes.py

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -15,4 +15,52 @@ def get_all_cards():
     
     response_body = [card.to_dict() for card in cards]
     return response_body
+
+@bp.get("/<card_id>")
+def get_card(card_id):
+    card = db.session.get(Card, card_id)
+    
+    response_body = {"card": card.to_dict()}
+    return response_body
+
+@bp.post("")
+def create_card():
+    request_body = request.get_json()
+    if "message" not in request_body:
+        response_body = {"details": "Invalid data"}
+        return make_response(response_body, 400)
+    
+    message = response_body["message"]
+    new_card = Card(message=message)
+    
+    db.session.add(new_card)
+    db.session.commit()
+    
+    response_body = {"new_card": new_card.to_dict()}
+    return response_body, 201
+    
+
+@bp.delete("/<card_id>")
+def delete_card(card_id):
+    card = db.session.get(Card, card_id)
+  
+    db.session.delete(card)
+    db.session.commit()
+    
+    response_body = {"details": f'Card {card_id} "{card.message}" successfully deleted.'}
+    
+    return response_body
+
+@bp.put("/<card_id>")
+def update_card(card_id):
+    card = db.session.get(Card, card_id)
+    
+    request_body = request.get_json()
+    card.message = request_body["message"]
+    
+    db.session.commit()
+    response_body = {"card": card.to_dict()}
+    
+    return make_response(response_body, 200)
+    
     
