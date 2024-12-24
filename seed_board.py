5e41c68886b07d9755d8ef6d730631afa1215fcc
anhtran <anhtr077@gmail.com>
2024-12-23 18:05:53 -0800

    create seed file for board

---
+++ seed_board.py
@@ -0,0 +1,11 @@
+from app import create_app, db
+from app.models.board import Board
+
+my_app = create_app()
+with my_app.app_context():
+    db.session.add(Board(title="Daily Motivation", owner="Anh"))
+    db.session.add(Board(title="Coding Wisdom", owner="Jenny"))
+    db.session.add(Board(title="Mindfulness Moments", owner="Brianna"))
+    db.session.add(Board(title="Life Lessons", owner="Anna"))
+   
+    db.session.commit()
